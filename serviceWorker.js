c700ee946692fcd89c05807004db6fbc5275e403
pd2 <its.pdp@gmail.com>
2025-05-26 17:32:22 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,9 +2,9 @@ const staticDev = "sahasranama"
 const service_worker_version = "v1.022"
 const assets = [
   "/",
-  "/index.html",
-  "/style.css",
-  "/script.js"
+  "index.html",
+  "style.css",
+  "script.js"
 ]
 
 self.addEventListener("install", installEvent => {
